2b33c245f6d49d88a423d8877db33ad9cc288036
timofeydem <368094@niuitmo.ru>
2022-10-26 21:42:40 +0300

    LFS and Submodule

--- main.c
+++ main.c
@@ -1,29 +1,3 @@
-#include <stdio.h>
-#include <string.h>
-int main(){
-    char first[11];
-    char second[11];
-    int n;
-    printf("Enter first string:\n");
-    scanf("%s", &first);
-
-
-    int size_of_first = strlen(first);
-    printf("Enter second string:\n");
-    scanf("%s", &second);
-    int size_of_second = strlen(second);
-    printf("Enter number of symbols:\n");
-    scanf("%d", &n);
-    char *third = strncat(first, second, n);
-
-    printf("First String + Second String[%d]: %s\n",n, third);
-
-    char * p = strrchr(first,'a');
-    printf ("Last occurence of 'a' found at %d \n", p - first + 1);
-    /* Он печатает все символы, которые есть в обеих строках. */
-    for ( p = first; *p; ++p )
-        if ( strchr(second, *p) )
-            printf("Result: %c\n", *p);
-    return 0;
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:55f11e673e04543f9d45a1176de4e4cebdb2ef038ec5e7c51b76bc031698d0c8
+size 817
